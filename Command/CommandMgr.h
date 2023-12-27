b1e59133447984d231c49c7779e5653fac3c001d
pro-antkeeper <153778081+pro-antkeeper@users.noreply.github.com>
2023-12-26 22:00:14 -0500

    Add files via upload

---
+++ Command/CommandMgr.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../../Memory/GameData.h"
+#include "Commands/BindCommand.h"
+#include "Commands/EjectCommand.h"
+#include "Commands/HelpCommand.h"
+#include "Commands/HideCommand.h"
+#include "Commands/ICommand.h"
+#include "Commands/ModulesCommand.h"
+#include "Commands/ServerCommand.h"
+#include "Commands/SpammerCommand.h"
+#include "Commands/ToggleCommand.h"
+#include "Commands/UnbindCommand.h"
+#include "Commands/ConfigCommand.h"
+#include "Commands/SetPrefixCommand.h"
+#include "Commands/WaypointCommand.h"
+#include "Commands/ScriptCommand.h"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class CommandMgr {
+private:
+	GameData* gameData;
+	std::vector<IMCCommand*> commandList;
+
+public:
+	CommandMgr(GameData* gm);
+	~CommandMgr();
+
+	char prefix = '!';
+
+	void initCommands();
+	void disable();
+	std::vector<IMCCommand*>* getCommandList();
+
+	void execute(char* message);
+};
+
+extern CommandMgr* cmdMgr;
