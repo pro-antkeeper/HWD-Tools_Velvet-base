fa3e40818bbb9ddc337a9e8798f1265633b68050
pro-antkeeper <153778081+pro-antkeeper@users.noreply.github.com>
2024-01-04 16:59:04 -0500

    Update AntiBot.h

--- HWD Tools/Horion/Module/Modules/AntiBot.h
+++ HWD Tools/Horion/Module/Modules/AntiBot.h
@@ -1,37 +1,27 @@
 #pragma once
+#include "../ModuleManager.h"
 #include "Module.h"
+
 class AntiBot : public IModule {
-private:
-	bool hitboxCheck = true;
-	bool nameCheck = false;
+public:
 	bool invisibleCheck = true;
 	bool entityIdCheck = true;
-	bool otherCheck = true;
+	bool hitboxCheck = true;
 	bool extraCheck = false;
+	bool nameCheck = false;
+	bool otherCheck = true;
 
-public:
+	std::string name = ("AntiBot");
+	SettingEnum mode = this;
+
+	bool isInvisibleCheckEnabled() { return invisibleCheck && this->isEnabled(); }
+	bool isEntityIdCheckEnabled() { return entityIdCheck && this->isEnabled(); }
+	bool isExtraCheckEnabled() { return this->extraCheck && this->isEnabled(); }
+	bool isHitboxCheckEnabled() { return hitboxCheck && this->isEnabled(); }
+	bool isOtherCheckEnabled() { return otherCheck && this->isEnabled(); }
+	bool isNameCheckEnabled() { return nameCheck && this->isEnabled(); }
+
+	virtual const char* getRawModuleName();
+	virtual const char* getModuleName();
 	AntiBot();
-	~AntiBot();
-
-	inline bool isHitboxCheckEnabled() {
-		return hitboxCheck && isEnabled();
-	}
-	inline bool isNameCheckEnabled() {
-		return false && isEnabled();
-	}
-	inline bool isInvisibleCheckEnabled() {
-		return invisibleCheck && isEnabled();
-	}
-	inline bool isEntityIdCheckEnabled() {
-		return entityIdCheck && isEnabled();
-	}
-	inline bool isOtherCheckEnabled() {
-		return otherCheck && isEnabled();
-	}
-	inline bool isExtraCheckEnabled() {
-		return extraCheck && isEnabled();
-	}
-
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
 };
