93207cb275f53bfbee83ad4a6cf12148ab21bd25
pro-antkeeper <153778081+pro-antkeeper@users.noreply.github.com>
2024-01-04 17:05:24 -0500

    Add files via upload

---
+++ HWD Tools/Utils/TargetUtil.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../Gamesense/Module/ModuleManager.h"
+#include "../SDK/CEntity.h"
+#include <vector>
+
+class TargetUtil
+{
+private:
+	static bool containsOnlyASCII(const std::string &string);
+
+public:
+	static void init(C_LocalPlayer **lc);
+	static bool isValidTarget(C_Entity *ent);
+};
