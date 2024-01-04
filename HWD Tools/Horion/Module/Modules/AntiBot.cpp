707818804aae527823caf65cfa65c2987d0855a0
pro-antkeeper <153778081+pro-antkeeper@users.noreply.github.com>
2024-01-04 16:59:27 -0500

    Update AntiBot.cpp

--- HWD Tools/Horion/Module/Modules/AntiBot.cpp
+++ HWD Tools/Horion/Module/Modules/AntiBot.cpp
@@ -1,17 +1,26 @@
 #include "AntiBot.h"
+#include "../../pch.h"
 
-AntiBot::AntiBot() : IModule(0, Category::PLAYER, "Enable this module to filter bots!") {
-	registerBoolSetting("Hitbox Check", &hitboxCheck, hitboxCheck);
-	//registerBoolSetting("Name Check", &nameCheck, nameCheck);
+using namespace std;
+AntiBot::AntiBot() : IModule(0, Category::PLAYER, "Ignores anticheat generated NPCs")
+{
+	registerEnumSetting("Mode", &mode, 0);
+	mode.addEntry(EnumEntry("Basic", 0));
+	mode.addEntry(EnumEntry("Advanced", 1));
 	registerBoolSetting("Invisible Check", &invisibleCheck, invisibleCheck);
 	registerBoolSetting("EntityID Check", &entityIdCheck, entityIdCheck);
+	registerBoolSetting("Hitbox Check", &hitboxCheck, hitboxCheck);
 	registerBoolSetting("Other Check", &otherCheck, otherCheck);
 	registerBoolSetting("Extra Check", &extraCheck, extraCheck);
 }
 
-AntiBot::~AntiBot() {
+const char* AntiBot::getRawModuleName()
+{
+	return "AntiBot";
 }
 
-const char* AntiBot::getModuleName() {
-	return ("AntiBot");
+const char* AntiBot::getModuleName()
+{
+	name = string("AntiBot ") + string(GRAY) + string("Advanced") + string(GRAY);
+	return name.c_str();
 }
