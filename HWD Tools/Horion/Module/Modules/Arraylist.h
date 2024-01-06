96ccd689f69da6d060a9e1008dcda6e94eef6a1c
pro-antkeeper <153778081+pro-antkeeper@users.noreply.github.com>
2024-01-05 20:00:05 -0500

    Update Arraylist.h

--- HWD Tools/Horion/Module/Modules/Arraylist.h
+++ HWD Tools/Horion/Module/Modules/Arraylist.h
@@ -1,18 +1,18 @@
 #pragma once
 #include "Module.h"
-
 class ArrayList : public IModule {
 public:
-	SettingEnum Modes;
 	ArrayList();
 	~ArrayList();
 
-	bool bottom = true;
-	float opacity = 0.2f;
-	bool bugs = false;
-	bool rgba = false;
-	bool Cool = false;
-	float scale = 1.0f;
+	bool rainbowArrayList = false;
+	bool bottom = false;
+	bool keybinds = true;
+	bool outline = false;
+	bool underline = false;
+	float scale = 1.f;
+	static float opacity;
 
+	// Inherited via IModule
 	virtual const char* getModuleName() override;
 };
